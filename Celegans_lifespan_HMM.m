6a71526c577e889e79f3ce80df5f3d14284ba2ec
Sangbin Han <37404101+sangbhan@users.noreply.github.com>
2018-06-09 23:38:15 +0900

    Add files via upload

--- Celegans_lifespan_HMM.m
+++ Celegans_lifespan_HMM.m
@@ -20,10 +20,10 @@ fileID = fopen(filename,'r');
 longlived_train = fscanf(fileID, formatSpec);
 fclose(fileID);
 
-STATE_NUM = 1:7;
+STATE_NUM = 1:3:19;
 EMISSION_NUM = 27;
 
-maxiter = 1000;
+maxiter = 100;
 tol = 1e-6;
 
 formatSpec = '%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f';
@@ -60,14 +60,12 @@ longlived_train_accuracy = zeros(1, 7);
 
 for j = 1:7
 
-    TRANS_INIT = ones(STATE_NUM(j), STATE_NUM(j))/STATE_NUM(j);
-    EMIS_INIT = ones(STATE_NUM(j), EMISSION_NUM)/EMISSION_NUM;
-    PSEUDOE = ones(STATE_NUM(j), EMISSION_NUM)/1e-4;
-    PSEUDOTR = ones(STATE_NUM(j), STATE_NUM(j))/1e-4;
+    TRANS_INIT = (ones(STATE_NUM(j), STATE_NUM(j)) + eye(STATE_NUM(j))) / (STATE_NUM(j) + 1);
+    EMIS_INIT = ones(STATE_NUM(j), EMISSION_NUM) / EMISSION_NUM;
     
-    [TRANS_short, EMIS_short] = hmmtrain(shortlived_train, TRANS_INIT, EMIS_INIT, 'tolerance', tol, 'maxiterations', maxiter);
-    [TRANS_normal, EMIS_normal] = hmmtrain(normallived_train, TRANS_INIT, EMIS_INIT, 'tolerance', tol, 'maxiterations', maxiter);
-    [TRANS_long, EMIS_long] = hmmtrain(longlived_train, TRANS_INIT, EMIS_INIT, 'tolerance', tol, 'maxiterations', maxiter);
+    [TRANS_short, EMIS_short] = hmmtrain(shortlived_train_a, TRANS_INIT, EMIS_INIT, 'tolerance', tol, 'maxiterations', maxiter);
+    [TRANS_normal, EMIS_normal] = hmmtrain(normallived_train_a, TRANS_INIT, EMIS_INIT, 'tolerance', tol, 'maxiterations', maxiter);
+    [TRANS_long, EMIS_long] = hmmtrain(longlived_train_a, TRANS_INIT, EMIS_INIT, 'tolerance', tol, 'maxiterations', maxiter);
     
     for i = 1:size(shortlived_train_a, 1)
 
@@ -85,7 +83,7 @@ for j = 1:7
 
     end
 
-    shortlived_train_accuracy(j) = shortlived_train_accuracy(j) / size(shortlived_train_a, 1);
+    shortlived_train_accuracy(j) = shortlived_train_accuracy(j) * 100 / size(shortlived_train_a, 1);
 
     for i = 1:size(shortlived_test_a, 1)
 
@@ -103,7 +101,7 @@ for j = 1:7
 
     end
 
-    shortlived_test_accuracy(j) = shortlived_test_accuracy(j) / size(shortlived_test_a, 1);
+    shortlived_test_accuracy(j) = shortlived_test_accuracy(j) * 100 / size(shortlived_test_a, 1);
 
     for i = 1:size(normallived_train_a, 1)
 
@@ -121,7 +119,7 @@ for j = 1:7
 
     end
 
-    normallived_train_accuracy(j) = normallived_train_accuracy(j) / size(normallived_train_a, 1);
+    normallived_train_accuracy(j) = normallived_train_accuracy(j) * 100 / size(normallived_train_a, 1);
 
     for i = 1:size(normallived_test_a, 1)
 
@@ -139,7 +137,7 @@ for j = 1:7
 
     end
 
-    normallived_test_accuracy(j) = normallived_test_accuracy(j) / size(normallived_test_a, 1);
+    normallived_test_accuracy(j) = normallived_test_accuracy(j) * 100 / size(normallived_test_a, 1);
 
     for i = 1:size(longlived_train_a, 1)
 
@@ -157,7 +155,7 @@ for j = 1:7
 
     end
 
-    longlived_train_accuracy(j) = longlived_train_accuracy(j) / size(longlived_train_a, 1);
+    longlived_train_accuracy(j) = longlived_train_accuracy(j) * 100 / size(longlived_train_a, 1);
 
     for i = 1:size(longlived_test_a, 1)
 
@@ -174,12 +172,152 @@ for j = 1:7
         end
 
     end
-
-    disp(logpseq)
     
-    longlived_test_accuracy(j) = longlived_test_accuracy(j) / size(longlived_test_a, 1);
+    longlived_test_accuracy(j) = longlived_test_accuracy(j) * 100 / size(longlived_test_a, 1);
+
+end
+
+plot(1:3:19, shortlived_train_accuracy, '-v', 1:3:19, shortlived_test_accuracy, '->', 1:3:19, normallived_train_accuracy, '-<', 1:3:19, normallived_test_accuracy, '-s', 1:3:19, longlived_train_accuracy, '-^', 1:3:19, longlived_test_accuracy, '-o')
+axis([0 20 0 70])
+legend('short-lived training accuracy', 'short-lived test accuracy', 'normal-lived training accuracy', 'normal-lived test accuracy', 'long-lived training accuracy', 'long-lived test accuracy')
+xlabel('Number of hidden states')
+ylabel('Accuracy (%)')
+title('Accuracy by number of hidden states', 'FontSize', 12)
+
+
+STATE_NUM = 4;
+TRANS_INIT = (ones(STATE_NUM, STATE_NUM) + eye(STATE_NUM)) / (STATE_NUM + 1);
+EMIS_INIT = ones(STATE_NUM, EMISSION_NUM) / EMISSION_NUM;
+
+maxiter = 1000;
+tol = [1e-1 1e-2 1e-3 1e-4 1e-5 1e-6 1e-7];
+
+shortlived_test_accuracy = zeros(1, 7);
+shortlived_train_accuracy = zeros(1, 7);
+normallived_test_accuracy = zeros(1, 7);
+normallived_train_accuracy = zeros(1, 7);
+longlived_test_accuracy = zeros(1, 7);
+longlived_train_accuracy = zeros(1, 7);
+
+for j = 1:7
+    
+    [TRANS_short, EMIS_short] = hmmtrain(shortlived_train_a, TRANS_INIT, EMIS_INIT, 'tolerance', tol(j), 'maxiterations', maxiter);
+    [TRANS_normal, EMIS_normal] = hmmtrain(normallived_train_a, TRANS_INIT, EMIS_INIT, 'tolerance', tol(j), 'maxiterations', maxiter);
+    [TRANS_long, EMIS_long] = hmmtrain(longlived_train_a, TRANS_INIT, EMIS_INIT, 'tolerance', tol(j), 'maxiterations', maxiter);
+    
+    for i = 1:size(shortlived_train_a, 1)
+
+        logpseq = [0 0 0];
+        [PSTATES, logpseq(1)] = hmmdecode(shortlived_train_a(i, :), TRANS_short, EMIS_short);
+        [PSTATES, logpseq(2)] = hmmdecode(shortlived_train_a(i, :), TRANS_normal, EMIS_normal);
+        [PSTATES, logpseq(3)] = hmmdecode(shortlived_train_a(i, :), TRANS_long, EMIS_long);
+        [M, A] = max(logpseq);
+
+        if A == 1
+
+            shortlived_train_accuracy(j) = shortlived_train_accuracy(j) + 1;
+
+        end
+
+    end
+
+    shortlived_train_accuracy(j) = shortlived_train_accuracy(j) * 100 / size(shortlived_train_a, 1);
+
+    for i = 1:size(shortlived_test_a, 1)
+
+        logpseq = [0 0 0];
+        [PSTATES, logpseq(1)] = hmmdecode(shortlived_test_a(i, :), TRANS_short, EMIS_short);
+        [PSTATES, logpseq(2)] = hmmdecode(shortlived_test_a(i, :), TRANS_normal, EMIS_normal);
+        [PSTATES, logpseq(3)] = hmmdecode(shortlived_test_a(i, :), TRANS_long, EMIS_long);
+        [M, A] = max(logpseq);
+
+        if A == 1
+
+            shortlived_test_accuracy(j) = shortlived_test_accuracy(j) + 1;
+
+        end
+
+    end
+
+    shortlived_test_accuracy(j) = shortlived_test_accuracy(j) * 100 / size(shortlived_test_a, 1);
+
+    for i = 1:size(normallived_train_a, 1)
+
+        logpseq = [0 0 0];
+        [PSTATES, logpseq(1)] = hmmdecode(normallived_train_a(i, :), TRANS_short, EMIS_short);
+        [PSTATES, logpseq(2)] = hmmdecode(normallived_train_a(i, :), TRANS_normal, EMIS_normal);
+        [PSTATES, logpseq(3)] = hmmdecode(normallived_train_a(i, :), TRANS_long, EMIS_long);
+        [M, A] = max(logpseq);
+
+        if A == 2
+
+            normallived_train_accuracy(j) = normallived_train_accuracy(j) + 1;
+
+        end
+
+    end
+
+    normallived_train_accuracy(j) = normallived_train_accuracy(j) * 100 / size(normallived_train_a, 1);
+
+    for i = 1:size(normallived_test_a, 1)
+
+        logpseq = [0 0 0];
+        [PSTATES, logpseq(1)] = hmmdecode(normallived_test_a(i, :), TRANS_short, EMIS_short);
+        [PSTATES, logpseq(2)] = hmmdecode(normallived_test_a(i, :), TRANS_normal, EMIS_normal);
+        [PSTATES, logpseq(3)] = hmmdecode(normallived_test_a(i, :), TRANS_long, EMIS_long);
+        [M, A] = max(logpseq);
+
+        if A == 2
+
+            normallived_test_accuracy(j) = normallived_test_accuracy(j) + 1;
+
+        end
+
+    end
+
+    normallived_test_accuracy(j) = normallived_test_accuracy(j) * 100 / size(normallived_test_a, 1);
+
+    for i = 1:size(longlived_train_a, 1)
+
+        logpseq = [0 0 0];
+        [PSTATES, logpseq(1)] = hmmdecode(longlived_train_a(i, :), TRANS_short, EMIS_short);
+        [PSTATES, logpseq(2)] = hmmdecode(longlived_train_a(i, :), TRANS_normal, EMIS_normal);
+        [PSTATES, logpseq(3)] = hmmdecode(longlived_train_a(i, :), TRANS_long, EMIS_long);
+        [M, A] = max(logpseq);
+
+        if A == 3
+
+            longlived_train_accuracy(j) = longlived_train_accuracy(j) + 1;
+
+        end
+
+    end
+
+    longlived_train_accuracy(j) = longlived_train_accuracy(j) * 100 / size(longlived_train_a, 1);
+
+    for i = 1:size(longlived_test_a, 1)
+
+        logpseq = [0 0 0];
+        [PSTATES, logpseq(1)] = hmmdecode(longlived_test_a(i, :), TRANS_short, EMIS_short);
+        [PSTATES, logpseq(2)] = hmmdecode(longlived_test_a(i, :), TRANS_normal, EMIS_normal);
+        [PSTATES, logpseq(3)] = hmmdecode(longlived_test_a(i, :), TRANS_long, EMIS_long);
+        [M, A] = max(logpseq);
+
+        if A == 3
+
+            longlived_test_accuracy(j) = longlived_test_accuracy(j) + 1;
+
+        end
+
+    end
+    
+    longlived_test_accuracy(j) = longlived_test_accuracy(j) * 100 / size(longlived_test_a, 1);
 
 end
 
 plot(1:7, shortlived_train_accuracy, '-v', 1:7, shortlived_test_accuracy, '->', 1:7, normallived_train_accuracy, '-<', 1:7, normallived_test_accuracy, '-s', 1:7, longlived_train_accuracy, '-^', 1:7, longlived_test_accuracy, '-o')
+axis([0 8 0 70])
 legend('short-lived training accuracy', 'short-lived test accuracy', 'normal-lived training accuracy', 'normal-lived test accuracy', 'long-lived training accuracy', 'long-lived test accuracy')
+xlabel('-log(tolerance)')
+ylabel('Accuracy (%)')
+title('Accuracy by -log(tolerance)', 'FontSize', 12)
